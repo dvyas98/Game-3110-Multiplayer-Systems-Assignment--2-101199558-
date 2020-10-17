1d374b871e964ed968b113ce3b0f14befa6e432a
dvyas98 <dhimant92@gmail.com>
2020-10-17 01:55:48 -0400

    A2

---
+++ Assets/Cube.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using NetworkObjects;
+using NetworkPlayer = NetworkObjects.NetworkPlayer;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class Cube : MonoBehaviour
+{
+
+    public string Address;
+    private GameObject _cube;
+    public Vector3 cubePos; 
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cube = GameObject.Find("NetworkClient");
+        InstantiateCube(_cube,cubePos);
+    }
+    public void deleteCube()
+    {
+        Destroy(gameObject);
+    }
+
+
+    public void InstantiateCube(GameObject Cube,Vector3 position)
+    {
+        Instantiate(_cube, position, Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       cubePos.x = this.transform.position.x;
+       cubePos.y = this.transform.position.y;
+       cubePos.z = this.transform.position.z;
+
+        //if (_cube.GetComponent<NetworkPlayer>().id == Address)
+        //{
+        //    _cube.GetComponent<NetworkPlayer>().cubPos = cubePos;
+
+        //    if (Input.GetKey(KeyCode.UpArrow))
+        //    {
+        //        //Debug.Log("Pressed");
+        //        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //    }
+        //    if (Input.GetKey(KeyCode.DownArrow))
+        //    {
+        //        //Debug.Log("Pressed");
+
+        //        transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+        //    }
+        //    if (Input.GetKey(KeyCode.LeftArrow))
+        //    {
+        //        // Debug.Log("Pressed");
+
+        //        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        //    }
+        //    if (Input.GetKey(KeyCode.RightArrow))
+        //    {
+        //        //Debug.Log("Pressed");
+
+        //        transform.Translate(-Vector3.left * Time.deltaTime * speed);
+        //    }
+        //}
+
+    }
+}
---
+++ Assets/Scripts/NetworkClient.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using Unity.Networking.Transport;
+using NetworkMessages;
+using NetworkObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NetworkPlayer = NetworkObjects.NetworkPlayer;
+
+public class NetworkClient : MonoBehaviour
+{
+    public NetworkDriver m_Driver;
+    public NetworkConnection m_Connection;
+    public string serverIP;
+    public ushort serverPort;
+    public NetworkObject player;
+    public List<string> newPlayerList;
+
+
+    void Start ()
+    {
+        m_Driver = NetworkDriver.Create();
+        m_Connection = default(NetworkConnection);
+        var endpoint = NetworkEndPoint.Parse(serverIP,serverPort);
+        m_Connection = m_Driver.Connect(endpoint);
+    }
+
+
+
+
+    void SendToServer(string message){
+        var writer = m_Driver.BeginSend(m_Connection);
+        NativeArray<byte> bytes = new NativeArray<byte>(Encoding.ASCII.GetBytes(message),Allocator.Temp);
+        writer.WriteBytes(bytes);
+        m_Driver.EndSend(writer);
+    }
+
+    void OnConnect(){
+        Debug.Log("We are now connected to the server");
+
+        //// Example to send a handshake message:
+         HandshakeMsg m = new HandshakeMsg();
+         m.player.id = m_Connection.InternalId.ToString();
+         player = new NetworkObject();
+         player.id = m.player.id;
+         newPlayerList.Add(player.id);
+         SpawnPlayers();
+         SendToServer(JsonUtility.ToJson(m));
+    }
+    void SpawnPlayers()
+    {
+        //Spawn the Cube
+        for (int i = 0; i <newPlayerList.Count; i++)
+        {
+            
+           
+        }
+        newPlayerList.Clear();
+        newPlayerList.TrimExcess();
+    }
+    void OnData(DataStreamReader stream){
+        NativeArray<byte> bytes = new NativeArray<byte>(stream.Length,Allocator.Temp);
+        stream.ReadBytes(bytes);
+        string recMsg = Encoding.ASCII.GetString(bytes.ToArray());
+        NetworkHeader header = JsonUtility.FromJson<NetworkHeader>(recMsg);
+
+        switch(header.cmd){
+            case Commands.HANDSHAKE:  //cmd 0
+            HandshakeMsg hsMsg = JsonUtility.FromJson<HandshakeMsg>(recMsg);
+            Debug.Log("Handshake message received!");
+            break;
+            case Commands.PLAYER_UPDATE:   //cmd 1
+            PlayerUpdateMsg puMsg = JsonUtility.FromJson<PlayerUpdateMsg>(recMsg);
+            Debug.Log("Player update message received!");
+            break;
+            case Commands.SERVER_UPDATE:   //cmd 2
+            ServerUpdateMsg suMsg = JsonUtility.FromJson<ServerUpdateMsg>(recMsg);
+            Debug.Log("Server update message received!");
+            break;
+            default:
+            Debug.Log("Unrecognized message received!");
+            break;
+        }
+    }
+
+    void Disconnect(){
+        m_Connection.Disconnect(m_Driver);
+        m_Connection = default(NetworkConnection);
+    }
+
+    void OnDisconnect(){
+        Debug.Log("Client got disconnected from server");
+        m_Connection = default(NetworkConnection);
+    }
+
+    public void OnDestroy()
+    {
+        m_Driver.Dispose();
+    }   
+    void Update()
+    {
+        m_Driver.ScheduleUpdate().Complete();
+
+        if (!m_Connection.IsCreated)
+        {
+            return;
+        }
+
+        DataStreamReader stream;
+        NetworkEvent.Type cmd;
+        cmd = m_Connection.PopEvent(m_Driver, out stream);
+        while (cmd != NetworkEvent.Type.Empty)
+        {
+            if (cmd == NetworkEvent.Type.Connect)
+            {
+                OnConnect();
+            }
+            else if (cmd == NetworkEvent.Type.Data)
+            {
+                OnData(stream);
+            }
+            else if (cmd == NetworkEvent.Type.Disconnect)
+            {
+                OnDisconnect();
+            }
+
+            cmd = m_Connection.PopEvent(m_Driver, out stream);
+        }
+    }
+}
